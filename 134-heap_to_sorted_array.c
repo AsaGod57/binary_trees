dc3b49e0b656d01aa4c3e135e2a1820e632c6765
Godsway <gasamoah@wacci.ug.edu.gh>
2024-03-27 13:28:40 -0700

    My heap134srtarray commit

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - Starts to convert a Binary Max Heap
+ * @heap: Specifies pointer to the root node of the heap to convert
+ * @size: Specifies address to store the size of the array
+ * Return: The array sorted (Ends)
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	return (heap);
+}
