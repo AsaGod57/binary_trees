4fa8ce2e61ef340dd2898d54baf947021e345d13
Godsway <gasamoah@wacci.ug.edu.gh>
2024-03-27 13:25:37 -0700

    My heap133extr commit

---
+++ 133-heap_extract.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - Starts to convert a Binary Max Heap
+ * @root: Specifies double pointer to the root node
+ * Return: The value stored in the root node (Ends)
+ */
+int heap_extract(heap_t **root)
+{
+	return (root);
+}
